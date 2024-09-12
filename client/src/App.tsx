b6a712e7bb37e4bf81cb25e2041a93866eca243c
Max <allenmaxnichols@gmail.com>
2024-09-12 11:52:38 -0500

    add side bar item and icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
 import "./App.css"
 import { Sidebar } from "./components/sidebar/sidebar"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
 
 const App = () => {
   return (
--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -1,10 +1,12 @@
 import styles from "./sidebar.module.css"
+import TitleIcon from "../../icons/title.svg"
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <p>Elements</p>
       <div className={styles["sidebar-section"]}>
         <div className={styles["sidebar-item"]}>
+          <img src={TitleIcon} />
           <p>Title</p>
         </div>
       </div>

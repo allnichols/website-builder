4411b87714f9fc4436cc542cee04912a46d35993
Max <allenmaxnichols@gmail.com>
2024-09-18 21:43:33 -0500

    remove unneeded imports

--- client/src/components/main-content/mainContent.tsx
+++ client/src/components/main-content/mainContent.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react"
-import { useDroppable, useDndContext } from "@dnd-kit/core"
+import { useDroppable } from "@dnd-kit/core"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 
 function MainContent() {
   const dispatch = useAppDispatch()
   const canvas = useAppSelector(state => state.canvas)
-  const { active, activeNode, activatorEvent } = useDndContext()
   const { isOver, setNodeRef } = useDroppable({
     id: "canvas",
     data: {
@@ -17,17 +15,14 @@ function MainContent() {
     backgroundColor: isOver ? "#f0f0f0" : "white",
   }
 
-  if (active) {
-    // console.log(active.data)
-  }
   return (
     <main className="content" ref={setNodeRef} style={style}>
-      {canvas.tree.map((element: any) => {
+      {canvas.tree.map((element: any, index: number) => {
         if (element.component === "title") {
-          return <h1 key={element.id}>title</h1>
+          return <h1 key={element.id + "-" + index}>title</h1>
         }
         if (element.component === "text") {
-          return <p key={element.id}>text</p>
+          return <p key={element.id + "-" + index}>text</p>
         }
       })}
     </main>
